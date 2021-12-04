56dc62d60765250ab0f051e99333f9b3c3ab7975
slen182 <87510893+slen182@users.noreply.github.com>
2021-12-04 12:48:19 -0500

    Add files via upload

---
+++ 01-java-fundamentals/01-Java First Program/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("My name is Steven Len");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Grand Ledge, MI");
+    }
+}
