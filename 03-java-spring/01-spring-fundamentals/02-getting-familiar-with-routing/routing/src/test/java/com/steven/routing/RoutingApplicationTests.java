4485f4342785a26cc8b3572fff1749bdfda9b6a8
slen182 <87510893+slen182@users.noreply.github.com>
2021-12-16 22:54:11 -0500

    Add files via upload

---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/routing/src/test/java/com/steven/routing/RoutingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.steven.routing;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
